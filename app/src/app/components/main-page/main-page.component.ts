29488d40a196b66cccb88d67ca2bf29006d71580
Sushant Sharma <ssushantlb08@gmail.com>
2022-12-26 04:58:26 +0530

    add marker feature
    new add marker feature

--- app/src/app/components/main-page/main-page.component.ts
+++ app/src/app/components/main-page/main-page.component.ts
@@ -8,44 +8,69 @@ import { environment } from 'src/environments/environment';
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+  
+  loc:object | undefined;
+  map: mapboxgl.Map | undefined;
+  style = 'mapbox://styles/mapbox/streets-v12';
+  lat = 0;
+  lng = 0;
+  zoom = 9;
 
-  map:mapboxgl.Map|undefined;
-  style= 'mapbox://styles/mapbox/streets-v12';
-  lat=40;
-  lng=-74.5;
-  zoom=9;
-  
-  
-  constructor() { 
+
+  constructor() {
     (mapboxgl as any).accessToken = environment.mapbox.accessToken;
   }
-  
+
   ngOnInit(): void {
     this.buildMap()
   }
   buildMap() {
     const navControl = new mapboxgl.NavigationControl({
-      visualizePitch:true
+      visualizePitch: true
     });
 
 
     this.map = new mapboxgl.Map({
       container: 'map', // container ID
       style: this.style, // style URL
-      center: [this.lng,this.lat], // starting position [lng, lat]
+      center: [this.lng, this.lat], // starting position [lng, lat]
       zoom: this.zoom, // starting zoom
-      attributionControl:false
+      attributionControl: false
     });
 
-    this.map.addControl(navControl,'top-right');
+    this.map.addControl(navControl, 'top-right');
     this.map.addControl(new mapboxgl.GeolocateControl({
-      positionOptions:{
-        enableHighAccuracy:true
+      positionOptions: {
+        enableHighAccuracy: true
       },
-      trackUserLocation:true,
-      showUserHeading:true
+      trackUserLocation: true,
+      showUserHeading: true
     }))
+    
+  }
+  
+  
+  addMarker(this: any) {
+      this.map.once('click', (e: { lngLat: { toArray: () => any; }; }) => {
+      this.loc=e.lngLat.toArray();
+      console.log(this.loc);
+      // this.arrayOfLoc.push(this.loc.lat)
+      // this.arrayOfLoc.push(this.loc.lng)
+
+
+      const marker = new mapboxgl.Marker({
+        color: "red",
+        draggable: true
+      })
+      .setLngLat(this.loc)
+      .addTo(this.map);
+      console.log(marker.getLngLat());
+      
+      });
+
+
+      
   }
 
-
 }
+
