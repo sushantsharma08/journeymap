53c0f256525c8bedf3712d0795cf2ba2bfcc637a
Sushant Sharma <ssushantlb08@gmail.com>
2022-12-26 01:42:01 +0530

    Update environment.prod.ts

--- app/src/environments/environment.prod.ts
+++ app/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  mapbox:{
+    accessToken:'pk.eyJ1Ijoic3VzaGFudDEyMzQ0MzIxIiwiYSI6ImNsYzNqNGYzYjFxaGMzcG1uM2N1dHhhZmMifQ.11-HLYmyWenelEBacVrP-g'
+  }
 };
